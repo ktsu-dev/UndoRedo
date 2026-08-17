5f0a0c1ca719ab6ee04b5e05137e65835ba92340
Matt Edmondson <matthew.edmondson@gmail.com>
2026-08-17 21:15:13 +1000

    Fix build errors from ktsu.Sdk analyzer updates (NU1506 duplicate PackageVersions, IDE0073 headers, IDE0055 line endings, KTSU0001/0002/0007) [patch]

--- UndoRedo.Test/UndoRedoStackTests.cs
+++ UndoRedo.Test/UndoRedoStackTests.cs
@@ -1,6 +1,4 @@
-// Copyright (c) ktsu.dev
-// All rights reserved.
-// Licensed under the MIT license.
+// Copyright (c) 2023-2026 ktsu-dev contributors
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
 
